71cb9a70566ca8af962a6799303aa322cf18d20b
sglee0730 <58541337+sglee0730@users.noreply.github.com>
2021-02-08 22:56:57 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        API_KEY: process.env.API_KEY || "//dapi.kakao.com/v2/maps/sdk.js?appkey=78432b1cd54e8c06459c90b6a1ae31ae"
+        API_KEY: process.env.API_KEY 
     }
 }
